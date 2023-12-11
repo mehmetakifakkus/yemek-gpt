fdd610ed2407a12a7b9073fb00480fd073c576a6
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-12-11 14:34:07 +0300

    Add chat route

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,38 @@
+import getChats from "@/app/actions/getChats";
+import ChatComponent from "@/components/ChatComponent";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    chatId: string;
+  };
+};
+
+const ChatPage = async ({ params: { chatId } }: Props) => {
+  const { userId } = auth();
+  const isAuth = !!userId;
+
+  if (!isAuth) {
+    return redirect("/sign-in");
+  }
+
+  return (
+    <div className="flex max-h-screen overflow-scroll">
+      <div className="flex w-full max-h-screen overflow-scroll">
+        {/* chat sidebar */}
+        {/* <div className="flex-[2] max-w-xs ">
+          <ChatSideBar chats={chats} chatId={chatId} />
+        </div> */}
+
+        {/* chat component */}
+        <div className="flex-[6] border-l-4 border-l-slate-400 px-[280px]">
+          <ChatComponent chatId={chatId} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
